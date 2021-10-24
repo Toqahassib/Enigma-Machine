d80eb52aef7d72d9b23da4c8efa5239deec2fed3
Toqahassib <70861082+Toqahassib@users.noreply.github.com>
2021-10-24 15:34:19 +0200

    Add files via upload

---
+++ cw1.py
@@ -0,0 +1,57 @@
+
+class Enigma:
+
+    def __init__(self):
+        pass
+
+    # Encodes a Message
+    def encode(self):
+        pass
+
+    # reset machine settings
+    def reset(self):
+        pass
+
+    # prints output
+    def print(self):
+        pass
+
+
+class Plugboard:
+
+    # pass through plug board
+    def __init__(self):
+        pass
+
+
+class Rotors:
+
+    def __init__(self, mappings, offset):
+        pass
+
+    # reset
+    def reset(self):
+        pass
+    # change ring settings
+
+    def configure(self):
+        pass
+
+    # forward
+    def forward(self):
+        pass
+
+    # reverse
+    def reverse(self):
+        pass
+
+    # rotate
+    def rotate(self):
+        pass
+
+
+class Reflector:
+
+    # forward
+    def forward(self):
+        pass

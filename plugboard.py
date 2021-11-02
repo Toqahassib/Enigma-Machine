feaada7b590ec1a2002fc24ffa5518a3028ed1c8
Toqahassib <70861082+Toqahassib@users.noreply.github.com>
2021-11-02 17:32:44 +0200

    plugboard file

---
+++ plugboard.py
@@ -0,0 +1,42 @@
+""" Plugboard pairings
+
+	Up to 10 alphabetic pairings could be made via physical wire connections.
+	This was the final component of the encryption before the accompanying lamp
+	would be lit for a keypress.
+
+	Taken from the 31st, 30th, and 29th from keylist #649, link below:
+	https://en.wikipedia.org/wiki/Enigma_machine#/media/File:Enigma_keylist_3_rotor.jpg
+"""
+
+plugboard31 = [('S', 'Z'),
+               ('G', 'T'),
+               ('D', 'V'),
+               ('K', 'U'),
+               ('F', 'O'),
+               ('M', 'Y'),
+               ('E', 'W'),
+               ('J', 'N'),
+               ('I', 'X'),
+               ('L', 'Q')]
+
+plugboard30 = [('I', 'S'),
+               ('E', 'V'),
+               ('M', 'X'),
+               ('R', 'W'),
+               ('D', 'T'),
+               ('U', 'Z'),
+               ('J', 'Q'),
+               ('A', 'O'),
+               ('C', 'H'),
+               ('N', 'Y')]
+
+plugboard29 = [('D', 'J'),
+               ('A', 'T'),
+               ('C', 'V'),
+               ('I', 'O'),
+               ('E', 'R'),
+               ('Q', 'S'),
+               ('L', 'W'),
+               ('P', 'Z'),
+               ('F', 'N'),
+               ('B', 'H')]
